5fa46ec6b06ff928fd654b877ae5f3dec7f38faf
tamaoki <treasure6hunter9cole@gmail.com>
2019-02-03 14:27:41 +0900

    first commit

---
+++ RxReader/RxReader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RxReader
+//
+//  Created by 玉置 on 2019/02/03.
+//  Copyright © 2019 Tamaoki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
