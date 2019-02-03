4dbdf54930b8c0e8c66853499cbd9054651b4106
tamaoki <treasure6hunter9cole@gmail.com>
2019-02-03 14:35:28 +0900

    pod install RxSwift and RxCocoa

---
+++ RxReader/Pods/Target Support Files/Pods-RxReader/Pods-RxReader-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RxReader : NSObject
+@end
+@implementation PodsDummy_Pods_RxReader
+@end
---
+++ RxReader/Pods/Target Support Files/Pods-RxReaderTests/Pods-RxReaderTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RxReaderTests : NSObject
+@end
+@implementation PodsDummy_Pods_RxReaderTests
+@end
