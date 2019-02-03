27135b60b16477eb7e07b06f7276346a6ff5f138
tamaoki <treasure6hunter9cole@gmail.com>
2019-02-03 16:43:24 +0900

    test imple tableview

--- RxReader/RxReader/Controllers/MainViewController.swift
+++ RxReader/RxReader/Controllers/MainViewController.swift
@@ -9,15 +9,37 @@
 import UIKit
 import RxSwift
 import RxCocoa
-import RxDataSources
+
+struct Person{
+    let name : String
+    let age : Int
+}
 
 class MainViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+
+    var personArr = [Person(name: "satish",age: 30),Person(name: "naresh",age: 28),Person(name: "lokesh",age: 40),Person(name: "nani",age: 25),Person(name: "hari",age: 30)]
+    
+    var objectArr : Observable<[Person]>?
+    
+    let disposeBag = DisposeBag()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let nib = UINib(nibName: "EntryTableViewCell", bundle: nil)
+        tableView.register(nib, forCellReuseIdentifier: "EntryTableViewCell")
+
+        objectArr = Observable.just(personArr)
+        
+        objectArr?.bind(to: tableView.rx.items(cellIdentifier:"EntryTableViewCell")){
+            _,person, cell in
+            if let cellToUse = cell as? EntryTableViewCell {
+                cellToUse.textLabel?.text = person.name
+                print(person.name)
+            }
+            }.disposed(by:disposeBag)
     }
-
-
 }
 
---
+++ RxReader/RxReader/Views/EntryTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  EntryTableViewCell.swift
+//  RxReader
+//
+//  Created by 玉置 on 2019/02/03.
+//  Copyright © 2019 Tamaoki. All rights reserved.
+//
+
+import UIKit
+
+class EntryTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
