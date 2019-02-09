b1f956c67f484ac451ff323906c32ef8822312c5
tamaoki <treasure6hunter9cole@gmail.com>
2019-02-09 23:00:35 +0900

    プログレスバーを実装する

--- RxReader/RxReader/Controllers/DetailPageViewController.swift
+++ RxReader/RxReader/Controllers/DetailPageViewController.swift
@@ -11,19 +11,43 @@ import WebKit
 
 class DetailPageViewController: UIViewController, WKUIDelegate {
     
+    var urlString: String?
+    
     private var webView: WKWebView = {
         let webConfiguration = WKWebViewConfiguration()
         return WKWebView(frame: .zero, configuration: webConfiguration)
     }()
-    
-    var urlString: String?
+
+    private var progressView: UIProgressView = {
+        let progressView = UIProgressView()
+        progressView.progressViewStyle = .bar
+        progressView.translatesAutoresizingMaskIntoConstraints = false
+        return progressView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         webView.uiDelegate = self
         view = webView
         requestPage()
+        
+        self.webView.addObserver(self, forKeyPath: "estimatedProgress", options: .new, context: nil)
+        self.webView.addObserver(self, forKeyPath: "loading", options: .new, context: nil)
+        setupProgressView()
+    }
+    
+    deinit {
+        self.webView.removeObserver(self, forKeyPath: "estimatedProgress", context: nil)
+        self.webView.removeObserver(self, forKeyPath: "loading", context: nil)
+    }
+    
+    private func setupProgressView() {
+        guard let navigationBar = self.navigationController?.navigationBar else { return }
+        navigationBar.addSubview(progressView)
+        self.progressView.leftAnchor.constraint(equalTo: navigationBar.leftAnchor, constant: 0).isActive = true
+        self.progressView.rightAnchor.constraint(equalTo: navigationBar.rightAnchor, constant: 0).isActive = true
+        self.progressView.bottomAnchor.constraint(equalTo: navigationBar.bottomAnchor, constant: 0).isActive = true
+        self.progressView.heightAnchor.constraint(equalToConstant: 3.0).isActive = true
     }
     
     private func requestPage() {
@@ -32,14 +56,22 @@ class DetailPageViewController: UIViewController, WKUIDelegate {
         let myRequest = URLRequest(url: myURL)
         webView.load(myRequest)
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if (keyPath == "estimatedProgress") {
+            self.progressView.alpha = 1.0
+            self.progressView.setProgress(Float(self.webView.estimatedProgress), animated: true)
+            if (self.webView.estimatedProgress >= 1.0) {
+                UIView.animate(withDuration: 0.3,
+                               delay: 0.3,
+                               options: [.curveEaseOut],
+                               animations: { [weak self] in
+                                self?.progressView.alpha = 0.0
+                    }, completion: {
+                        (finished : Bool) in
+                        self.progressView.setProgress(0.0, animated: false)
+                })
+            }
+        }
     }
-    */
-
 }
