14a559c9f7c3021ba73118d7a426aaf728c3e162
tamaoki <treasure6hunter9cole@gmail.com>
2019-02-09 22:37:34 +0900

    Loggerライブラリを追加してLogを仕込む

--- RxReader/Pods/Target Support Files/Pods-RxReader/Pods-RxReader-frameworks.sh
+++ RxReader/Pods/Target Support Files/Pods-RxReader/Pods-RxReader-frameworks.sh
@@ -144,6 +144,7 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/CocoaLumberjack/CocoaLumberjack.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Nuke/Nuke.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/RxAtomic/RxAtomic.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/RxCocoa/RxCocoa.framework"
@@ -151,6 +152,7 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/CocoaLumberjack/CocoaLumberjack.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Nuke/Nuke.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/RxAtomic/RxAtomic.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/RxCocoa/RxCocoa.framework"
--- RxReader/RxReader/Models/EntryApiClient.swift
+++ RxReader/RxReader/Models/EntryApiClient.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Alamofire
+import CocoaLumberjack
 
 class EntryApiClient: NSObject {
     
@@ -16,63 +17,32 @@ class EntryApiClient: NSObject {
     struct Path {
         static let index = "/api/index"
     }
-//    func request(completion: @escaping ([Entry]?, Error?) -> Void) {
-//        guard let url = URL(string: "https://summary-app-api.herokuapp.com/api/index") else { return }
-//        let task = URLSession.shared.dataTask(with: url) { data, response, error in
-//            if let error = error {
-//                print("クライアントエラー: \(error.localizedDescription) \n")
-//                completion(nil, error)
-//                return
-//            }
-//            guard let data = data, let response = response as? HTTPURLResponse else {
-//                print("no data or no response")
-//                return
-//            }
-//            if response.statusCode == 200 {
-//                print(data)
-//                let decoder: JSONDecoder = JSONDecoder()
-//                do {
-//                    let json = try decoder.decode(ResponseData.self, from: data)
-//                    print(json)
-//                    completion(json.data, nil)
-//                } catch {
-//                    print("error:", error.localizedDescription)
-//                    completion(nil, error)
-//                }
-//            } else {
-//                print("サーバエラー ステータスコード: \(response.statusCode)\n")
-//                completion(nil, error)
-//            }
-//        }
-//        task.resume()
-//    }
     
     func request(parameters: [String: AnyObject]?, completion: @escaping ([Entry]?, Error?) -> Void) {
         guard let url = URL(string: baseUrl + Path.index) else { return }
         Alamofire.request(url, method: .get, parameters: parameters)
             .responseJSON { response in
-                debugPrint(response)
+                DDLogInfo(response.description)
                 switch response.result {
                 case .success:
-                    print("Success!")
+                    DDLogInfo("Success!")
                     guard let data = response.data else {
-                        print("no data or no response")
+                        DDLogInfo("no data or no response")
                         return
                     }
                     if response.response?.statusCode == 200 {
-                        print(data)
+                        DDLogInfo(data.description)
                         let decoder: JSONDecoder = JSONDecoder()
                         do {
                             let json = try decoder.decode(ResponseData.self, from: data)
-                            print(json)
                             completion(json.data, nil)
                         } catch {
-                            print("error:", error.localizedDescription)
+                            DDLogError("error: \(error.localizedDescription)")
                             completion(nil, error)
                         }
                     }
                 case .failure:
-                    print("Failure!")
+                    DDLogError("Failure!")
                 }
         }
     }
